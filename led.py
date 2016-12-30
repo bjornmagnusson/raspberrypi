6640bfd80c2717619a6bbd91465057edacf6ce13
Björn Magnusson <bjorn.magnusson@infinera.com>
2016-12-30 13:29:57 +0100

    Fixed missing declarations

--- led.py
+++ led.py
@@ -4,11 +4,9 @@ import time
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(7, GPIO.OUT)
 
-num_blinks = 0
-
 num_blinks = input("Number of blinks?")
 
-for i in range(count):
+for i in range(num_blinks):
 	print "Lighting" + str(i)
 	GPIO.output(7,True)
 	time.sleep(1)
