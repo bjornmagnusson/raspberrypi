e3cbcda53e74e7d06bdc10141e09874d32f465d2
Björn Magnusson <bjorn.magnusson@infinera.com>
2016-12-31 15:55:35 +0100

    Error handling

--- led.go
+++ led.go
@@ -8,16 +8,22 @@ import "time"
 var pin = rpio.Pin(4)
 
 func main() {
-    if (err := rpio.Open(): err != nil) {
+    fmt.Println("Opening rpio")
+
+    var err = rpio.Open()
+
+    if err != nil {
         fmt.Println(err)
         os.Exit(1)
     }
 
     defer rpio.Close()
 
+    fmt.Println("Pin as output")
     pin.Output()
 
     for i := 0; i < 20; i++ {
+        fmt.Println("Toggle")
         pin.Toggle()
         time.Sleep(time.Second)
     }
