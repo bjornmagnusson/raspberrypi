e1086a3a61937fa1f370c20286e1f8844fcf4d6e
Björn Magnusson <bjorn.magnusson@infinera.com>
2017-01-04 00:29:11 +0100

    python: iterating all pins

--- led.py
+++ led.py
@@ -1,17 +1,21 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(7, GPIO.OUT)
+pins = [4, 17, 27]
+
+GPIO.setmode(GPIO.BCM)
+for pin in pins:
+	GPIO.setup(pin, GPIO.OUT)
 
 num_blinks = input("Number of blinks?")
 
 for i in range(num_blinks):
 	print "Lighting" + str(i)
-	GPIO.output(7,True)
-	time.sleep(1)
-	GPIO.output(7,False)
-	time.sleep(1)
+	for pin in pins:
+		GPIO.output(pin,True)
+		time.sleep(1)
+		GPIO.output(pin,False)
+		time.sleep(1)
 
 print "Done"
 
