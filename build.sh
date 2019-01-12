b14ca668f634ce3f9ac6172142a8ad171d6a6f6f
Björn Magnusson <bjornmagnusson1984@gmail.com>
2019-01-12 23:50:59 +0100

    Docker: use buildkit above 18.09

--- build.sh
+++ build.sh
@@ -2,5 +2,13 @@
 set -e
 
 TAG=${1:-latest}
-DOCKER_BUILDKIT=1 docker image build --target test .
-DOCKER_BUILDKIT=1 docker image build -t bjornmagnusson/pi-led .
+BUILDKIT_ENABLED=1
+
+DOCKER_SERVER_VERSION=$(docker version --format '{{ .Server.Version }}')
+DOCKER_CLIENT_VERSION=$(docker version --format '{{ .Client.Version }}')
+if [[ $DOCKER_SERVER_VERSION < 18.09 || $DOCKER_CLIENT_VERSION < 18.09 ]]; then
+  BUILDKIT_ENABLED=0
+fi
+
+DOCKER_BUILDKIT=$BUILDKIT_ENABLED docker image build --target test .
+DOCKER_BUILDKIT=$BUILDKIT_ENABLED docker image build -t bjornmagnusson/pi-led .
